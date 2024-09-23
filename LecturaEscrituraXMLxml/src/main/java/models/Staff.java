f3a43555424d3d90eef9cd4386429a1c667964fa
DvdPires19 <david.pires1997@gmail.com>
2024-09-23 16:47:31 +0200

    Clase Staff creada. Atributos, constructor, getters y setters y toString creados

---
+++ LecturaEscrituraXMLxml/src/main/java/models/Staff.java
@@ -0,0 +1,67 @@
+package models;
+
+public class Staff {
+	
+	protected String firstName;
+	protected String lastName;
+	protected String nickname;
+	protected int salary;
+	protected String currency;
+	
+	public Staff(String firstName, String lastName, String nickname, int salary, String currency) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.nickname = nickname;
+		this.salary = salary;
+		this.currency = currency;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	@Override
+	public String toString() {
+		return "Staff\nFirst Name:" + firstName + "\nLastName: " + lastName + "Nickname: " + nickname + "\nSalary: "
+				+ salary + currency;
+	}
+	
+	
+	
+}
