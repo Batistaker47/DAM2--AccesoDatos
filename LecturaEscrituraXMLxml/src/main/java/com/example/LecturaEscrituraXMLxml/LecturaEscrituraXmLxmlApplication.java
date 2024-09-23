ce741d494395de22f89062ba81f4848bafeab320
DvdPires19 <david.pires1997@gmail.com>
2024-09-23 16:26:01 +0200

    Ejercicio 2 de lectura de un documento xml creado

---
+++ LecturaEscrituraXMLxml/src/main/java/com/example/LecturaEscrituraXMLxml/LecturaEscrituraXmLxmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.LecturaEscrituraXMLxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecturaEscrituraXmLxmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecturaEscrituraXmLxmlApplication.class, args);
+	}
+
+}
