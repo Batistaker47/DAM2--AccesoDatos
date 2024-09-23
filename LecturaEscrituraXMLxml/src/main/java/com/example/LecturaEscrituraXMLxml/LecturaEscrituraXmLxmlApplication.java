2396cff77709c281c88d8b643da136ee6de06fb8
DvdPires19 <david.pires1997@gmail.com>
2024-09-23 16:50:22 +0200

    Cambio en la estructura del proyecto

--- LecturaEscrituraXMLxml/src/main/java/com/example/LecturaEscrituraXMLxml/LecturaEscrituraXmLxmlApplication.java
+++ LecturaEscrituraXMLxml/src/main/java/com/example/LecturaEscrituraXMLxml/LecturaEscrituraXmLxmlApplication.java
@@ -1,13 +1,14 @@
 package com.example.LecturaEscrituraXMLxml;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class LecturaEscrituraXmLxmlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LecturaEscrituraXmLxmlApplication.class, args);
+		System.out.println("Hola!");
+		//SpringApplication.run(LecturaEscrituraXmLxmlApplication.class, args);
 	}
 
 }
