153ec0b9e99e78fb35d1523853ac2bce34666fee
DvdPires19 <david.pires1997@gmail.com>
2024-09-26 17:33:14 +0200

    Proyecto de parseo de PDF a XML creado

---
+++ LecturaEscrituraPDFxml/src/main/java/com/example/LecturaEscrituraPDFxml/LecturaEscrituraPdFxmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.LecturaEscrituraPDFxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecturaEscrituraPdFxmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecturaEscrituraPdFxmlApplication.class, args);
+	}
+
+}
