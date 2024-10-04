f248a6c6bd5ea95eed949ccf565f9b2209b3cb0b
DvdPires19 <david.pires1997@gmail.com>
2024-10-04 17:17:37 +0200

    Proyecto de parseo de word a txt inicializado

---
+++ LecturaEscrituraWordtxt/LecturaEscrituraWordtxt/src/main/java/com/example/LecturaEscrituraWordtxt/LecturaEscrituraWordtxtApplication.java
@@ -0,0 +1,13 @@
+package com.example.LecturaEscrituraWordtxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecturaEscrituraWordtxtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecturaEscrituraWordtxtApplication.class, args);
+	}
+
+}
