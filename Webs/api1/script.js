a9d5ec43bf70e84abee5ca01ba513263be865847
batistaker47 <david.pires1997@gmail.com>
2025-01-14 11:21:27 +0100

    Todo Ok en el formulario

--- Webs/api1/script.js
+++ Webs/api1/script.js
@@ -8,11 +8,12 @@ boton.addEventListener("click",evento => {
     registrarAlumno();
 });
 
-fields.name = document.getElementById("name").value;
-fields.surname = document.getElementById("surname").value;
-fields.career = document.getElementById("career").value;
-
 let registrarAlumno = async()=> {
+    
+    fields.name = document.getElementById("name").value;
+    fields.surname = document.getElementById("surname").value;
+    fields.career = document.getElementById("career").value;
+
     const peticion = await fetch("http://localhost:8080/api/alumnos",
     {
         method: 'POST',
